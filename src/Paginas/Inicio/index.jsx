5b969f0054c9f834be82370573c9d12eef3141b4
edpadua <ed.padua@gmail.com>
2023-05-24 23:11:48 -0300

    Correções nos arquivos .sass
    Correções nos arquivos .sass

--- src/Paginas/Inicio/index.jsx
+++ src/Paginas/Inicio/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './Inicio.module.sass';
 
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector } from "../../../../node_modules/react-redux/" ;
 
 
 
